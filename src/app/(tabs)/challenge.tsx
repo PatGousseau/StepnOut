17e5e64671184252b5e6de615f9e9d9712967bcd
Zach Gousseau <zgousseau@gmail.com>
2024-11-30 15:12:12 -0800

    fix merge issue

--- src/app/(tabs)/challenge.tsx
+++ src/app/(tabs)/challenge.tsx
@@ -14,6 +14,7 @@ import { useActiveChallenge } from '../../hooks/useActiveChallenge';
 import { Challenge } from '../../types';
 import { Loader } from '../../components/Loader';
 import ShareChallenge from '../../components/ShareChallenge';
+import { ScrollView, RefreshControl } from 'react-native';
 
 
 interface ChallengeCardProps {
