718b9cacb7021a38c87707d2d16eeed34ee244ce
Pat Gousseau <pcgousseau@gmail.com>
2024-12-26 16:22:40 -0600

    notifications for test flight

---
+++ stepnout/stepnout/stepnoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  stepnoutApp.swift
+//  stepnout
+//
+//  Created by Pat Gousseau  on 2024-12-26.
+//
+
+import SwiftUI
+
+@main
+struct stepnoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ stepnout/stepnoutTests/stepnoutTests.swift
@@ -0,0 +1,16 @@
+//
+//  stepnoutTests.swift
+//  stepnoutTests
+//
+//  Created by Pat Gousseau  on 2024-12-26.
+//
+
+import Testing
+
+struct stepnoutTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
