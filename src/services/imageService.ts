8ed4738b4d681622bdc66fe07cdaac4bc7af7f58
Zach Gousseau <zgousseau@gmail.com>
2025-02-23 13:34:01 -0800

    display images as non-square

--- src/services/imageService.ts
+++ src/services/imageService.ts
@@ -99,10 +99,10 @@ export const imageService = {
     }
 
     const sizeMap = {
-      small: { quality: 60, width: 400, height: 400 },
-      medium: { quality: 70, width: 800, height: 800 },
-      large: { quality: 80, width: 1200, height: 1200 },
-      preview: { quality: 10, width: 20, height: 20 },
+      small: { quality: 60 },
+      medium: { quality: 70 },
+      large: { quality: 80 },
+      preview: { quality: 10 },
     };
 
     const [previewUrl, fullUrl] = await Promise.all([
